50659219451d0aad86d16129f638b5ce93067aba
edanmora95 <edison_andre_9@hotmail.com>
2018-03-23 17:05:39 -0500

    Front estudiantes-preguntar

--- client/estudiantes/src/App.vue
+++ client/estudiantes/src/App.vue
@@ -1,8 +1,19 @@
 <template>
-  <div id="app">
-    <img src="./assets/logo.png">
-    <router-view/>
-  </div>
+  <v-app id="app">
+    <v-toolbar app dark>
+      <v-toolbar-side-icon></v-toolbar-side-icon>
+      <v-toolbar-title class="white--text">Ask The Teacher</v-toolbar-title>
+    </v-toolbar>
+    <v-content>
+      <v-tabs centered grow>
+        <v-tab :to="{name:'Preguntar'}">Preguntar</v-tab>
+        <v-tab :to="{name:'Responder'}">Responder</v-tab>
+      </v-tabs>
+      <v-container>
+        <router-view/>
+      </v-container>
+    </v-content>
+  </v-app>
 </template>
 
 <script>
@@ -18,6 +29,5 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
--- client/estudiantes/src/main.js
+++ client/estudiantes/src/main.js
@@ -1,11 +1,22 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
+import Vuetify from 'vuetify'
+import moment from 'moment'
+import 'vuetify/dist/vuetify.min.css' // Ensure you are using css-loader
 import App from './App'
 import router from './router'
 
+Vue.use(Vuetify)
+
 Vue.config.productionTip = false
 
+// Filtro para mostrar las fechas
+Vue.filter('moment', (value) => {
+  if (value) {
+    return moment(value).format('LTS')
+  }
+})
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
