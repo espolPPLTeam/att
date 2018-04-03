dd2ceafab8e775ab45414587ac7c1a804060201a
Joel Rodriguez <joelerll@gmail.com>
2018-04-03 16:34:06 -0500

    add sockets preguntar profesor

--- api/sockets.js
+++ api/sockets.js
@@ -6,14 +6,30 @@ module.exports = function({ io, shortid, logger }) {
     sockets.push({ socketId, socket })
     socket.on('unirseAParalelo', function({ paraleloId }) {
       socket.join(`${paraleloId}`)
+      // TODO: eliminar el true enviado
       socket.emit('UNIDO_PARALELO', true)
     })
     socket.on('preguntaEstudiante', function({ preguntaId, texto, paraleloId, createdAt, creador: { _id, correo, matricula, nombres, apellidos } }) {
       Socket.in(`${paraleloId}`).emit('PREGUNTA_ESTUDIANTE', { preguntaId, texto, paraleloId, createdAt, creador: { _id, correo, matricula, nombres, apellidos } })
     })
+    /*
+     * el profesor hace una pregunta y debe ser enviado a todos los profesores ademas de los estudiantes
+    */
+    // FIX: si una pregunta esta hecha y corriendo, no debe poder hacerse otra
+    socket.on('preguntaProfesor', function({ paraleloId, preguntaId, texto, creador: { nombres, apellidos, tipo, correo } }) {
+      Socket.in(`${paraleloId}`).emit('PREGUNTA_PROFESOR', { paraleloId, preguntaId, texto, creador:{ nombres, apellidos, tipo, correo } })
+    })
+    // lo que el estudiante envia al profesor
+    socket.on('responder', function({ paraleloId, texto, preguntaId, createdAt, creador: { nombres, apellidos, tipo, correo } }) {
+      Socket.in(`${paraleloId}`).emit('RESPONDER', { paraleloId, texto, preguntaId, createdAt, creador: { nombres, apellidos, tipo, correo } })
+    })
+    socket.on('terminarPregunta', function({ paraleloId, preguntaId, terminadaPor: { nombres, apellidos, tipo, correo } }) {
+     Socket.in(`${paraleloId}`).emit('TERMINAR_PREGUNTA', { paraleloId, preguntaId, terminadaPor: { nombres, apellidos, tipo, correo } }) 
+    })
     socket.on('disconnect', function() {
       const CANTIDAD_CONECTADOS = Object.keys(io.sockets.connected).length
       logger.info(`cantidad-usuarios-conectados ${CANTIDAD_CONECTADOS}`)
+      Socket.emit('CANTIDAD_CONECTADOS', CANTIDAD_CONECTADOS)
       let socketDesconectado = sockets.find(socketObjeto => socketId == socketObjeto.socketId)
       sockets = sockets.filter(socketObjeto => socketId != socketObjeto.socketId)
       if (socketDesconectado) {
--- api/test/sockets.test.js
+++ api/test/sockets.test.js
@@ -11,7 +11,9 @@ describe('Sockets', () => {
   beforeEach(function(done){
     server.listen(process.env.PORT || 3000, function() {
       profesor = io(`http://localhost:${process.env.PORT || 3000}/att`, ioOptions)
+      profesor_2 = io(`http://localhost:${process.env.PORT || 3000}/att`, ioOptions)
       estudiante = io(`http://localhost:${process.env.PORT || 3000}/att`, ioOptions)
+      estudiante_2 = io(`http://localhost:${process.env.PORT || 3000}/att`, ioOptions)
       done()
     })
 
@@ -19,16 +21,14 @@ describe('Sockets', () => {
   afterEach(function(){
     profesor.disconnect()
     estudiante.disconnect()
+    profesor_2.disconnect()
+    estudiante_2.disconnect()
     server.close()
   })
   after(function() {
     server.close()
   })
   it('@t1 ESTUDIANTE HACE UNA PREGUNTA', function(done) {
-    this.timeout(6000)
-    let doc = {
-
-    }
     let estudianteData = data.estudiantes[0]
     let paraleloId = 'pppp'
     let texto = 'Mi primera pregunta'
@@ -54,6 +54,98 @@ describe('Sockets', () => {
       }
       expect(preguntaRecibida).to.deep.equal(pregunta)
       done()
+    })
+  })
+  describe('@t2 PROFESOR PREGUNTA', () => {
+    let paraleloId = 'idPa'
+    let paraleloId_2 = 'idPa2'
+    let texto = 'Mi pregunta profesor'
+    let preguntaId = 'idPre'
+    let profesorData = data.profesores[0]
+    let profesor_2Data = data.profesores[1]
+    let preguntaProfesor = { paraleloId, preguntaId, texto, creador: profesorData }
+    it('@t2.1 ENVIADA A LOS ESTUDIANTES DEL PARALELO', (done) => {
+      estudiante.on('connect', function() {
+        estudiante.emit('unirseAParalelo', { paraleloId })
+        estudiante.on('PREGUNTA_PROFESOR', function({ paraleloId, preguntaId, texto, creador }) {
+          expect({ paraleloId, preguntaId, texto, creador }).to.deep.equal(preguntaProfesor)
+          done()
+        })
+      })
+      profesor.on('connect', function() {
+        profesor.emit('unirseAParalelo', { paraleloId })
+        profesor.on('UNIDO_PARALELO', function() {
+          profesor.emit('preguntaProfesor', preguntaProfesor)
+        })
+      })
+    })
+    it('@t2.2 ENVIADA OTRO PROFESOR DEL PARALELO', (done) => {
+      profesor_2.on('connect', function() {
+        profesor_2.emit('unirseAParalelo', { paraleloId })
+        profesor_2.on('PREGUNTA_PROFESOR', function({ paraleloId, preguntaId, texto, creador }) {
+          expect({ paraleloId, preguntaId, texto, creador }).to.deep.equal(preguntaProfesor)
+          done()
+        })
+      })
+      profesor.on('connect', function() {
+        profesor.emit('unirseAParalelo', { paraleloId })
+        profesor.on('UNIDO_PARALELO', function() {
+          profesor.emit('preguntaProfesor', preguntaProfesor)
+        })
+      })
+    })
+    it('@t2.3 ESTUDIANTE DE OTRO PARALELO NO DEBE RECIBIRLA', (done) => {
+      estudiante_2.on('connect', function() {
+        estudiante_2.emit('unirseAParalelo', { paraleloId_2 })
+        estudiante_2.on('PREGUNTA_PROFESOR', function({ paraleloId, preguntaId, texto, creador }) {
+          expect(true).to.equal(false)
+        })
+      })
+      profesor.on('connect', function() {
+        profesor.emit('unirseAParalelo', { paraleloId })
+        profesor.on('UNIDO_PARALELO', function() {
+          profesor.emit('preguntaProfesor', preguntaProfesor)
+        })
+      })
+      setTimeout(done, 600)
+    }).timeout(1000)
+    it('@t2.4 PROFESOR DE OTRO PARALELO NO DEBE RECIBIRLA', function(done) {
+      this.timeout(1000)
+      profesor_2.on('connect', function() {
+        profesor_2.emit('unirseAParalelo', { paraleloId_2 })
+        profesor_2.on('PREGUNTA_PROFESOR', function({ paraleloId, preguntaId, texto, creador }) {
+          expect(true).to.equal(false)
+        })
+      })
+      profesor.on('connect', function() {
+        profesor.emit('unirseAParalelo', { paraleloId })
+        profesor.on('UNIDO_PARALELO', function() {
+          profesor.emit('preguntaProfesor', preguntaProfesor)
+        })
+      })
+      setTimeout(done, 600)
+    })
+    it('@t2.5 PROFESOR TERMINA PREGUNTA Y EL ESTUDIANTE LO RECIBE', function(done) {
+      let terminada = { paraleloId, preguntaId, texto, terminadaPor: profesor_2Data }
+      estudiante.on('connect', function() {
+        estudiante.emit('unirseAParalelo', { paraleloId })
+        estudiante.on('TERMINAR_PREGUNTA', function({ paraleloId, preguntaId, terminadaPor: { nombres, apellidos, tipo, correo } }) {
+          expect({ paraleloId, preguntaId, texto, terminadaPor: { nombres, apellidos, tipo, correo } }).to.deep.equal(terminada)
+          done()
+        })
+      })
+      profesor.on('connect', function() {
+        profesor.emit('unirseAParalelo', { paraleloId })
+        profesor.on('UNIDO_PARALELO', function() {
+          profesor.emit('preguntaProfesor', preguntaProfesor)
+        })
+      })
+      profesor_2.on('connect', function() {
+        profesor_2.emit('unirseAParalelo', { paraleloId: paraleloId })
+        profesor_2.on('PREGUNTA_PROFESOR', function({ paraleloId, preguntaId, texto, creador }) {
+          profesor_2.emit('terminarPregunta', terminada)
+        })
+      })
     })
   })
 })
